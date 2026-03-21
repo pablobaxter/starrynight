3ea1634e95045eda321cbdcbbfd97411e8e029c8
Pablo Baxter <pablobaxter@msn.com>
2026-03-20 17:59:29 -0700

    Make everything isolated project safe (for now)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,14 +6,3 @@ buildscript {
         }
     }
 }
-
-plugins {
-    id("com.frybits.plugin")
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.kotlinx.serialization) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.android.cache.fix) apply false
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,15 +8,31 @@ pluginManagement {
     plugins {
         id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
         id("com.android.settings") version providers.gradleProperty("com.frybits.agp.version")
+
+        id("com.android.application") version providers.gradleProperty("com.frybits.agp.version")
+        id("com.android.library") version providers.gradleProperty("com.frybits.agp.version")
+        id("org.jetbrains.kotlin.android") version providers.gradleProperty("com.frybits.kotlin.version")
+        id("org.jetbrains.kotlin.jvm") version providers.gradleProperty("com.frybits.kotlin.version")
+        id("org.jetbrains.kotlin.plugin.serialization") version "2.3.20"
+        id("com.google.devtools.ksp") version providers.gradleProperty("com.frybits.ksp.version")
+        id("org.gradle.android.cache-fix") version "3.0.3"
     }
+
+    includeBuild("build-logic")
 }
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention")
     id("com.android.settings")
+    id("com.frybits.plugin") apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
+    id("org.jetbrains.kotlin.jvm") apply false
+    id("org.jetbrains.kotlin.plugin.serialization") apply false
+    id("com.google.devtools.ksp") apply false
+    id("org.gradle.android.cache-fix") apply false
 }
-
-includeBuild("build-logic")
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
@@ -44,3 +60,8 @@ val projectList: Provider<Array<String>> = providers.fileContents(layout.rootDir
 include(*projectList.get())
 
 rootProject.name = providers.gradleProperty("com.frybits.name").get()
+
+@Suppress("UnstableApiUsage")
+gradle.lifecycle.beforeProject {
+    apply(plugin = "com.frybits.plugin")
+}
