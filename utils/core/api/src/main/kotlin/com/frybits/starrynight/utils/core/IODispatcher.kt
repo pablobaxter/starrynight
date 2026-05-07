09d9bf22fc1a722b11061325cb005c5d42d66cf2
Pablo Baxter <pablobaxter@msn.com>
2026-05-06 23:33:57 -0700

    Attempt to fix duplicate binding issue

--- utils/core/api/src/main/kotlin/com/frybits/starrynight/utils/core/IODispatcher.kt
+++ utils/core/api/src/main/kotlin/com/frybits/starrynight/utils/core/IODispatcher.kt
@@ -20,14 +20,5 @@ package com.frybits.starrynight.utils.core
 
 import dev.zacsweers.metro.Qualifier
 
-@Target(
-    AnnotationTarget.CLASS,
-    AnnotationTarget.FIELD,
-    AnnotationTarget.FUNCTION,
-    AnnotationTarget.PROPERTY,
-    AnnotationTarget.PROPERTY_GETTER,
-    AnnotationTarget.VALUE_PARAMETER,
-    AnnotationTarget.TYPE,
-)
 @Qualifier
 public annotation class IODispatcher
